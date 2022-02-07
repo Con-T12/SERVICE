6dfe0af7e3659ab39c35edb692a6233571be007c
Con-T12 <86234612+Con-T12@users.noreply.github.com>
2022-02-07 20:45:46 +1030

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBPibWm38FfcUL5zNxVitrTqHKcesWmmqw",
+  authDomain: "f5-time-cfc2e.firebaseapp.com",
+  databaseURL: "https://f5-time-cfc2e-default-rtdb.firebaseio.com",
+  projectId: "f5-time-cfc2e",
+  storageBucket: "f5-time-cfc2e.appspot.com",
+  messagingSenderId: "448596814233",
+  appId: "1:448596814233:web:386a319c029a9a2b028b08"
+};
+
+
+const app = initializeApp(firebaseConfig);
